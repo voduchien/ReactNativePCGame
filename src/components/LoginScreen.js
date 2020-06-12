e59c45a86165cb9f4bd244ce5a3eff46590aaea9
voduchien <44290271+voduchien@users.noreply.github.com>
2020-06-12 23:49:15 +0700

    add login screen

---
+++ src/components/LoginScreen.js
@@ -0,0 +1,10 @@
+import React,{Component} from "react"
+import {View,Text} from "react-native"
+import Wallpaper from './Wallpaper';
+export default class LoginScreen extends Component{
+render(){
+    return (
+        <Wallpaper></Wallpaper>
+    )
+}
+}
